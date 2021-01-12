bb9d0d2ed1424feda734400fca24d78b7ecd34f2
Adam Cameron Drummond <66436400+AdamDrummondIOM@users.noreply.github.com>
2021-01-12 12:34:20 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RandomNumberGenerator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                DateTime currentTime = DateTime.Now;
+                Console.Write(Math.Acos(-1).ToString().Substring(3)[currentTime.Millisecond%10]);
+                Console.Write(Math.Asin(-1).ToString().Substring(3)[currentTime.Second%10]);
+                Console.Write(Math.Atan(-1).ToString().Substring(3)[(currentTime.Millisecond*currentTime.Second)%10]);
+                Console.ReadLine();
+            }
+        }
+    }
+}
